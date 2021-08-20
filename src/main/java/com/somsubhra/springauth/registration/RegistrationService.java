df3142d1397aff35ff6fd7593a5f2484f82e70e7
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-20 13:20:48 +0530

    added annotations and fixed bugs

--- src/main/java/com/somsubhra/springauth/registration/RegistrationService.java
+++ src/main/java/com/somsubhra/springauth/registration/RegistrationService.java
@@ -1,5 +1,10 @@
 package com.somsubhra.springauth.registration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
